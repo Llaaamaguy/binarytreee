d206453962ee95eb3856e88e36a86ef2a940e7bc
Llaaamaguy <67341568+Llaaamaguy@users.noreply.github.com>
2022-05-25 14:42:24 -0400

    working insert and iter

--- binarytree.py
+++ binarytree.py
@@ -28,29 +28,40 @@ class BinarySearchTree:
         # This method should iterate over the left side of the tree, then
         # return the val, then iterate over the right side of the tree.
         def __iter__(self):
-            # TODO: implement this.
-            pass
+            if self.getLeft() is not None:
+                for i in self.getLeft():
+                    yield i
+            yield self.val
+
+            if self.getRight() is not None:
+                for i in self.getRight():
+                    yield i
 
     def __init__(self):
         self.root = None
 
     # Insert something into the tree.
-    def insert(self, val)
-
+    def insert(self, val):
         # The __insert function is recursive and is not passed a self
         # parameter. It is static function (not a method of the class) but
         # is hidden inside so users of the class will not know it exists.
         def __insert(root, val):
-            # TODO: implement this
-            pass
+            if root is None:
+                return self.__Node(val)
+            else:
+                if val <= root.getVal():
+                    return self.__Node(root.getVal(), __insert(root.getLeft(), val), root.getRight())
+                else:
+                    return self.__Node(root.getVal(), root.getLeft(), __insert(root.getRight(), val))
 
         self.root = __insert(self.root, val)
 
     def __iter__(self):
-        if self.root != None:
+        if self.root is not None:
             return self.root.__iter__()
         else:
             return [].__iter__()
+
 
 def main():
     s = input("Enter a list of numbers, seperated by spaces: ")
@@ -62,6 +73,7 @@ def main():
 
     for x in tree:
         print(x)
+
 
 if __name__ == "__main__":
     main()
